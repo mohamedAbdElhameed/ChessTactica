88e34b5d8cdef1756b8e24f3b4db505aef71a33b
MohamedAbdElhameed <mohamedabdelhameed34@gmail.com>
2017-10-10 15:36:30 +0200

    fix the board in portrait mode and delete unnecessary code

--- app/src/main/java/com/example/mohamedabdelhameed/chesstactica/Board.java
+++ app/src/main/java/com/example/mohamedabdelhameed/chesstactica/Board.java
@@ -14,45 +14,42 @@ import android.view.View;
 
 public class Board extends View {
 
-	private int boardSize, squareSide;
-	private boolean isSet = true;
-	private Paint color;
+    private int boardSize, squareSide;
+    private boolean isSet = true;
+    private Paint color;
 
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		boardSize = canvas.getWidth();
-		squareSide = boardSize / 8;
-		drawBoard(canvas);
-	}
-	
-	public void drawBoard(Canvas c) {
-		for (int i = 0; i < boardSize; i += squareSide) {
-			isSet = !isSet;
-			for (int j = 0; j < boardSize; j += squareSide) {
-				color.setColor(isSet ? Color.BLACK : Color.WHITE);
-				c.drawRect(new Rect(i, j, i + squareSide, j + squareSide), color);
-				isSet = !isSet;
-			}
-		}
-	}
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        final int width = canvas.getWidth();
+        final int height = canvas.getHeight();
+        boardSize = Math.min(width, height);
+        squareSide = boardSize / 8;
+        drawBoard(canvas);
+    }
 
-	public Board(Context context) {
-		super(context);
-		setFocusable(true);
-		setFocusableInTouchMode(true);
-		requestFocus();
-		color = new Paint(Paint.ANTI_ALIAS_FLAG);
-		color.setColor(Color.BLACK);
-	}
+    public void drawBoard(Canvas c) {
+        int countRow = 0;
+        int countCol = 0;
+        if (c.getWidth() > c.getHeight()) {
+            isSet = !isSet;
+        }
+        for (int i = 0; i < boardSize && countCol < 8; i += squareSide) {
+            isSet = !isSet;
+            countCol++;
+            for (int j = 0; j < boardSize && countRow < 8; j += squareSide) {
+                color.setColor(isSet ? Color.BLACK : Color.WHITE);
+                c.drawRect(i, j, i + squareSide, j + squareSide, color);
+                isSet = !isSet;
+                countRow++;
+            }
+            countRow = 0;
+        }
+    }
 
     public Board(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        setFocusable(true);
-        setFocusableInTouchMode(true);
-        requestFocus();
-        color = new Paint(Paint.ANTI_ALIAS_FLAG);
-        color.setColor(Color.BLACK);
+        color = new Paint();
     }
 }
